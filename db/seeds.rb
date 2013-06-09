0bbd92d9b433213a2e209ab8e93c2ce1386557bd
shal23 <dr.shalinidalal@gmail.com>
2013-06-09 19:09:53 +1000

    change

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
-# See http://railsapps.github.io/rails-environment-variables.html
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
-end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.confirm!
+# See http://railsapps.github.com/rails-environment-variables.html
+puts 'CREATING ROLES'
+Role.create([
+  { :name => 'admin' }, 
+  { :name => 'user' },
+  { :name => 'VIP' }
+], :without_protection => true)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'User1', 
+	:email => 'user@example.com', 
+	:password => 'pleaseme', 
+	:password_confirmation => 'pleaseme'
 user.add_role :admin
+
+puts 'New user created: ' << user.name
+user2 = User.create! :name => 'User2', :email => 'user2@example.com', :password => 'pleaseme', :password_confirmation => 'pleaseme'
+puts 'New user created: ' << user2.name
+
+user2.add_role :student
